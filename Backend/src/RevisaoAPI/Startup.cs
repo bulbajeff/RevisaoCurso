f7563556460113e687c2f0440fc763e6f123c13d
Jefferson Sambuichi <samuraijeff@outlook.com>
2023-04-14 16:40:44 -0300

    RevisaoAPI

--- Backend/src/RevisaoAPI/Startup.cs
+++ Backend/src/RevisaoAPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RevisaoAPI.Data;
 
 namespace RevisaoAPI
 {
@@ -26,7 +27,7 @@ namespace RevisaoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(context => context.UseSqlServer);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
